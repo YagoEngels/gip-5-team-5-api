05d562bdae27d41ea0b5c53b863ce6441b3e8a01
PC001 laptop <yago.engels@gmail.com>
2023-02-20 19:22:36 +0100

    Initial commit

---
+++ src/main/java/com/example/gip5team5api/Gip5Team5ApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gip5team5api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gip5Team5ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gip5Team5ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gip5team5api/Gip5Team5ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gip5team5api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gip5Team5ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
