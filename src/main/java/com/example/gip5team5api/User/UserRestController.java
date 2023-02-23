4e5c2f5ad066b9a57be191878ea462a675b20d90
sander <sander.raymakers@student.ucll.be>
2023-02-23 22:06:22 +0100

    Inventory aangepast en UserRestController aangemaakt.

--- src/main/java/com/example/gip5team5api/User/UserRestController.java
+++ src/main/java/com/example/gip5team5api/User/UserRestController.java
@@ -1,4 +1,43 @@
 package com.example.gip5team5api.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/")
 public class UserRestController {
+
+    @Autowired
+    public UserService userService;
+
+    @PutMapping("/user/{id}")
+    public void updateUser(@PathVariable long id, @RequestBody User user){
+        userService.updateUser(id,user);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/user")
+    public User addUser(@RequestBody User user){
+        userService.add(user);
+        return user;
+    }
+
+    @GetMapping("user/{id}")
+    public Optional<User> getUserById(@PathVariable long id) { return userService.findById(id); }
+
+
+    @RequestMapping("/user")
+    public Iterable<User> getUsers() { return userService.findAll(); }
+
+    @DeleteMapping("/user/{id}")
+    public void removeUser(@PathVariable long id) {
+        userService.removeUser(userService.findById(id).get());
+    }
+
+
+
 }
+
