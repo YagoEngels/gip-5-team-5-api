e8f09c53191647fff7c3483b275545ee456d38f0
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2023-02-23 20:38:11 +0100

    item rrest controler gedaan

--- src/main/java/item/ItemRestController.java
+++ src/main/java/item/ItemRestController.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/")
 public class ItemRestController {
-    //hier alle stories
+    //hier alle stories geda
 }
