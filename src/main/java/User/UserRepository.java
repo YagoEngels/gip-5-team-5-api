dce28959d9337ddd5df05e1c5295fe7957bc34b8
sander <sander.raymakers@student.ucll.be>
2023-02-23 20:31:34 +0100

    User repository

--- src/main/java/User/UserRepository.java
+++ src/main/java/User/UserRepository.java
@@ -1,4 +1,11 @@
 package User;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    List<User> findAll();
 }
