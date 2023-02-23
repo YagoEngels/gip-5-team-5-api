fd3981e0bfb801f271f05aa4a3b7b8d35d5f677f
sander <sander.raymakers@student.ucll.be>
2023-02-23 20:32:12 +0100

    User class

--- src/main/java/User/User.java
+++ src/main/java/User/User.java
@@ -1,4 +1,68 @@
 package User;
 
+import jakarta.persistence.*;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+@Table(name="User")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    String firstname;
+
+    @NotNull
+    String lastname;
+
+    @NotNull
+    String email;
+
+    @NotNull
+    String birthdate;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public User() {
+    }
+
+    public User( String firstname,String lastname,  String email,  String birthdate) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.birthdate = birthdate;
+    }
 }
